d188c7612b5e88214f379cb680f4ac579b021afd
Andrés Toledo Cervera <145764095+poblano10@users.noreply.github.com>
2023-10-18 22:41:29 +0200

    Update DyV.h

--- DyV.h
+++ DyV.h
@@ -22,5 +22,38 @@ int BusquedaBinaria(T x,const vector<T>& a,int ini,int fin){
 		return BusquedaBinaria(x,a,medio+1,fin);
         }
 }
+int sort(vector<T>& a,int ini,int fin){
+
+        int i = ini;
+        T x = a[fin];
+
+
+        for(int j=ini;j<fin;j++){
+                if(a[j] <= x){
+                        swap(a,i,j);
+                        i++;
+                }
+        }
+        swap(a,i,fin);
+        return i;
+}
+
+
+void swap(vector<T>& a,int f,int g){
+
+    T aux =a[f];
+    a[f] = a[g];
+    a[g] = aux;
+}
+void QuickSort(vector<T>& a,int ini,int fin){
+
+        if(ini<fin){
+                int pivote= sort(a,ini,fin);
+                QuickSort(a,ini,pivote-1);
+                QuickSort(a,pivote+1,fin);
+
+        }
+
+}
 
 
